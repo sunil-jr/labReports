086394f8b4c1b7b7028435602964ae571209e502
sunilsubedi <subedisunil495@gmail.com>
2024-01-06 16:34:26 +0545

    lab 4

---
+++ src/labsheet4/q4/SendMail.java
@@ -0,0 +1,29 @@
+package labsheet4.q4;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMail {
+    public static void main(String[] args) {
+        String to = "abcd@gmail.com";
+        String from = "web@gmail.com";
+        String host = "localhost";
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("This is the Subject Line!");
+            message.setText("This is actual message");
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
+
